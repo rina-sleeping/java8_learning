0744ca604876cc4568b006207a4d2441cc0fc6eb
Rina TAKAHASHI <rina2mistletoe@gmail.com>
2014-08-14 23:10:55 +0900

    ch1/ex12: add.

---
+++ ch1/ex12/Ex12.java
@@ -0,0 +1,17 @@
+package ex12;
+
+
+public class Ex12 {
+
+	// private class Stream<T> {
+	//
+	// }
+	//
+	// private class ArrayList2<T> extends ArrayList<T> {
+	// private static final long serialVersionUID = 1L;
+	//
+	// public Stream<T> stream() {
+	// return new Stream<T>();
+	// }
+	// }
+}
