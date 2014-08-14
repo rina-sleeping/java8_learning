45e96ddc7dabd1d984900b60353a2b622e85f01a
Rina TAKAHASHI <rina2mistletoe@gmail.com>
2014-08-14 11:55:04 +0900

    ch1/ex3: add files.

---
+++ ch1/ex3/FileLamdaTest.java
@@ -0,0 +1,48 @@
+package ex3;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class FileLamdaTest {
+	
+	@Test
+	public void testLamda() {
+		File pwd = new File("ch1/ex3/test");
+		File a =new File("ch1/ex3/test/a.txt");
+		File b = new File("ch1/ex3/test/b.txt");
+		File c = new File("ch1/ex3/test/c.prn");
+		
+		try {
+			pwd.mkdir();
+			a.createNewFile();
+			b.createNewFile();
+			c.createNewFile();
+			
+			File[] list;
+			list = FileLamda.getSubDiectoriesByLamda(pwd.getPath(),"txt");
+			
+			assertNotNull(list);
+			assertEquals(2,list.length);
+			assertEquals(a,list[0]);
+			assertEquals(b,list[1]);
+			
+			list = FileLamda.getSubDiectoriesByLamda(pwd.getPath(),"prn");
+			
+			assertNotNull(list);
+			assertEquals(1,list.length);
+			assertEquals(c,list[0]);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Exception is occured");
+		} finally{
+			a.delete();
+			b.delete();
+			c.delete();
+			pwd.delete();
+		}	
+	}
+}
