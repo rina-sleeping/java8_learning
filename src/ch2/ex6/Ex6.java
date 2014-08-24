01891846d6ae8e576c8b43b458837ffee108ae8b
Rina TAKAHASHI <rina2mistletoe@gmail.com>
2014-08-24 20:25:53 +0900

    src/ch2/ex6: add.

---
+++ src/ch2/ex6/Ex6.java
@@ -0,0 +1,9 @@
+package ch2.ex6;
+
+import java.util.stream.Stream;
+
+public class Ex6 {
+	public static Stream<Character> characterStream(String s) {
+		return Stream.iterate(0, n -> n + 1).limit(s.length()).map(s::charAt);
+	}
+}
