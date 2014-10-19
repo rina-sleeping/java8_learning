1b261a1d19dd7cac4a636cbd5e2f56ea5b7f8fb7
Rina TAKAHASHI <rina2mistletoe@gmail.com>
2014-10-19 23:59:05 +0900

    src/ch4/ex6: add.

---
+++ src/ch4/ex6/BorderPaneDemo.java
@@ -0,0 +1,25 @@
+package ch4.ex6;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class BorderPaneDemo extends Application {
+	public void start(Stage stage) {
+		BorderPane pane = new BorderPane();
+		Button top = new Button("Top");
+		BorderPane.setAlignment(top, Pos.TOP_CENTER);
+		pane.setTop(top);
+		pane.setLeft(new Button("Left"));
+		pane.setCenter(new Button("Center"));
+		pane.setRight(new Button("Right"));
+		Button bottom = new Button("Bottom");
+		BorderPane.setAlignment(bottom, Pos.BOTTOM_CENTER);
+		pane.setBottom(bottom);
+		stage.setScene(new Scene(pane));
+		stage.show();
+	}
+}
