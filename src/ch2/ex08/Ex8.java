c0777f14b480a05f7d9cc08fc5903f4fe226d5f1
Rina TAKAHASHI <rina2mistletoe@gmail.com>
2014-10-25 23:30:47 +0900

    src/ch2/ex08: add zip2.

--- src/ch2/ex08/Ex8.java
+++ src/ch2/ex08/Ex8.java
@@ -1,5 +1,8 @@
 package ch2.ex08;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -18,5 +21,60 @@ public class Ex8 {
 		}
 
 		return combined.stream();
+	}
+
+	public static <T> Stream<T> zip2(Stream<T> first, Stream<T> second) {
+		if (!isFinite(first) && !isFinite(second)) {
+			Iterator<T> fIte = first.iterator();
+			Iterator<T> sIte = second.iterator();
+			StreamSupplier<T> supplier = new StreamSupplier<T>(fIte, sIte);
+			return Stream.generate(supplier::getNext);
+		} else {
+			return zip(first, second);
+		}
+	}
+
+	public static <T> boolean isFinite(Stream<T> stream) {
+		try {
+			Field f = stream.getClass().getSuperclass().getSuperclass()
+					.getDeclaredField("sourceSpliterator");
+			f.setAccessible(true);
+			Object splite = f.get(stream);
+
+			Method mSize = splite.getClass().getMethod("getExactSizeIfKnown");
+			mSize.setAccessible(true);
+			Long size = (Long) mSize.invoke(splite);
+
+			return size != -1;
+		} catch (NoSuchMethodException | SecurityException
+				| IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
+
+class StreamSupplier<T> {
+	private int count;
+	private Iterator<T> fIte;
+	private Iterator<T> sIte;
+
+	StreamSupplier(Iterator<T> fIte, Iterator<T> sIte) {
+		this.count = 0;
+		this.fIte = fIte;
+		this.sIte = sIte;
+	}
+
+	T getNext() {
+		count++;
+		if (!fIte.hasNext() || !sIte.hasNext()) {
+
+		}
+		if (count % 2 == 1) {
+			return fIte.next();
+		} else {
+			return sIte.next();
+		}
 	}
 }
--- src/ch2/ex08/Ex8Test.java
+++ src/ch2/ex08/Ex8Test.java
@@ -30,4 +30,30 @@ public class Ex8Test {
 				Stream.generate(Math::random));
 		assertEquals(6, result2.count());
 	}
+
+	@Test
+	public void test2() {
+		Stream<Integer> result = Ex8.zip2(Stream.of(1, 3, 5),
+				Stream.of(2, 4, 6, 7));
+		assertArrayEquals(Stream.of(1, 2, 3, 4, 5, 6).toArray(),
+				result.toArray());
+
+		result = Ex8.zip2(Stream.of(2, 4, 6, 7), Stream.of(1, 3, 5));
+		assertArrayEquals(Stream.of(2, 1, 4, 3, 6, 5).toArray(),
+				result.toArray());
+
+		result = Ex8.zip2(Stream.of(1, 3, 5), Stream.empty());
+		assertArrayEquals(Stream.empty().toArray(), result.toArray());
+
+		result = Ex8.zip2(Stream.empty(), Stream.of(1, 3, 5));
+		assertArrayEquals(Stream.empty().toArray(), result.toArray());
+
+		Stream<Double> result2 = Ex8.zip2(Stream.of(1.2, 3.0, 5.0),
+				Stream.generate(Math::random));
+		assertEquals(6, result2.count());
+
+		result2 = Ex8.zip2(Stream.generate(Math::random),
+				Stream.generate(Math::random));
+		assertEquals(-1, result2.spliterator().getExactSizeIfKnown());
+	}
 }
