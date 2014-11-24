cbd8c513e45ad403f773925b06a1ee78716c2a7f
Rina TAKAHASHI <rina2mistletoe@gmail.com>
2014-11-24 18:46:34 +0900

    src/ch6/ex08: add.

---
+++ src/ch6/ex08/Ex8.java
@@ -0,0 +1,50 @@
+package ch6.ex08;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
+
+public class Ex8 {
+
+	public static void main(String[] args) {
+		Random random = new Random(new Date().getTime());
+		final int ADD = 100000;
+		final int LOOP = 5;
+		long start, end;
+
+		int size = 0;
+		for (int c = 0; c < 5; c++) {
+			size += ADD;
+			int[] data = new int[size];
+
+			for (int i = 0; i < size; ++i) {
+				data[i] = random.nextInt(size);
+			}
+
+			System.out.printf("lenght=%d\n", size);
+
+			long sum = 0;
+			for (int i = 0; i < LOOP; i++) {
+				int[] tmp = data.clone();
+				start = System.nanoTime();
+				Arrays.parallelSort(tmp);
+				end = System.nanoTime();
+				sum += end - start;
+			}
+			System.out.println("Arrays.parallelSort\t:" + sum / LOOP);
+
+			sum = 0;
+			for (int i = 0; i < LOOP; i++) {
+				int[] tmp = data.clone();
+				start = System.nanoTime();
+				Arrays.sort(tmp);
+				end = System.nanoTime();
+				sum += end - start;
+			}
+
+			System.out.println("Arrays.sort\t\t:" + sum / LOOP);
+
+			System.out.println("----------------");
+		}
+	}
+}
