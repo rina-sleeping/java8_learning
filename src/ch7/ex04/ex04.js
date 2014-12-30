5a9577ae67bee4b47ec1b9a13f1ff530f6176787
Rina TAKAHASHI <rina2mistletoe@gmail.com>
2014-12-30 12:21:49 +0900

    ch7/ex04: add.

---
+++ src/ch7/ex04/ex04.js
@@ -0,0 +1,9 @@
+// Run as jjs < sortDisplay.js
+
+var str = 'AAA BBBB'
+var s =  str.slice(4)
+print(s.getClass())
+
+java.lang.String.class.cast(s.getClass())
+//java.lang.String.class.cast(s) //‚±‚Á‚¿‚È‚çOK
+
